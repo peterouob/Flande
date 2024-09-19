d91fdcb01cc212844d18a967b26dd2381d5cc77b
林庭楷 <hellolinpeter@gmail.com>
2024-09-19 14:40:42 +0800

    change:router service

--- router/router.go
+++ router/router.go
@@ -2,13 +2,13 @@ package router
 
 import (
 	"ecomm/logger"
-	"ecomm/service"
+	"ecomm/service/user"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(r *gin.Engine) {
 	l := logger.NewLogger()
 	r.Use(logger.GinLogger(l), logger.GinRecovery(l, true))
-	r.POST("/login", service.LoginUser)
-	r.POST("/create", service.CreateUser)
+	r.POST("/login", user.LoginUser)
+	r.POST("/create", user.CreateUser)
 }
