8616e3e7326be2e16869853ac141daa578493911
林庭楷 <hellolinpeter@gmail.com>
2024-09-23 09:56:23 +0800

    feat: cors

--- router/router.go
+++ router/router.go
@@ -3,12 +3,14 @@ package router
 import (
 	"ecomm/logger"
 	"ecomm/service/user"
+	"ecomm/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(r *gin.Engine) {
 	l := logger.NewLogger()
 	r.Use(logger.GinLogger(l), logger.GinRecovery(l, true))
+	r.Use(utils.Cors)
 	r.POST("/login", user.LoginUser)
 	r.POST("/create", user.CreateUser)
 }
